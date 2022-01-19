65851da35bdb560611158b62fdc008e09da2141d
root <root@localhost>
2022-01-19 17:30:40 +0800

    init

---
+++ precache-manifest.c586b547c5f8861a87e34a0c146539c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/lotterya/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/lotterya/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "b37d7741d7060a22444858f6af5c7dd8",
+    "url": "/lotterya/static/media/banner2.b37d7741.jpg"
+  },
+  {
+    "revision": "45810c841dfbe1b083f2",
+    "url": "/lotterya/static/js/runtime~main.45810c84.js"
+  },
+  {
+    "revision": "5998a16af71fd2bc649e",
+    "url": "/lotterya/static/js/main.5998a16a.chunk.js"
+  },
+  {
+    "revision": "dc5738324b6b2a48d5c0",
+    "url": "/lotterya/static/js/1.dc573832.chunk.js"
+  },
+  {
+    "revision": "5998a16af71fd2bc649e",
+    "url": "/lotterya/static/css/main.d798c0cc.chunk.css"
+  },
+  {
+    "revision": "dc5738324b6b2a48d5c0",
+    "url": "/lotterya/static/css/1.14a83e06.chunk.css"
+  },
+  {
+    "revision": "b085ba04be737815858d35e53ef14393",
+    "url": "/lotterya/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lottery_game/precache-manifest.58ec499fafec4dd4fbef4cd4b7d12f27.js"
+  "/lotterya/precache-manifest.c586b547c5f8861a87e34a0c146539c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lottery_game/index.html", {
+workbox.routing.registerNavigationRoute("/lotterya/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
